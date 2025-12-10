3cb5313537b4d86b4b28dfdf3a7743479054b288
Javier Ladino <javierladino@me.com>
2025-12-10 14:09:42 +0100

    First release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Review Release",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Javier Ladino",                         # Tu nombre
+    author_email="javierladino@me.com",                 # Tu correo electrónico
+    url="https://github.com/javiladino/review_release",     # URL del proyecto
+)
